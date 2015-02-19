53d9d084131a4c950fb4784b1a69dd44adcd06b2
AndrewMore <masentertainmentinc.ent@gmail.com>
2015-02-19 16:31:26 -0500

    initial commit

---
+++ assn-2-2.py
@@ -0,0 +1,3 @@
+prompt = "What is your name?"
+name = raw_input (prompt)
+print "Hello", name
---
+++ assn-2-5.py
@@ -0,0 +1,4 @@
+#Convert Celsius to Fahrenheit
+Celsius = float (raw_input("Enter degrees in Celsius:"))
+Fahrenheit = (Celsius * 9) / 5 + 32
+print "F", Fahrenheit
